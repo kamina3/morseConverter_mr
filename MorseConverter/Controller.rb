e70862af6ae8f70e7f0537d89d9e8c8b9e73e89b
ash1taka <ashitaka77@gmail.com>
2012-11-14 12:09:29 +0900

    init ruby file

---
+++ MorseConverter/Controller.rb
@@ -0,0 +1,22 @@
+#
+#  Controller.rb
+#  MorseConverter
+#
+#  Created by neige7 on 12/11/14.
+#  Copyright 2012年 neige7. All rights reserved.
+#
+#require './MorseCode.rb'
+
+class Controller
+    
+    attr_accessor :button , :orgform , :kanaform, :alpform
+    def initialize
+        @mc = MorseCode.new
+        puts "initialize"
+    end
+    
+    def button_pressd(sender)
+        puts "the button pressed!"
+        orgform.setStringValue("button pressed!")
+    end
+end
---
+++ MorseConverter/MorseCode.rb
@@ -0,0 +1,18 @@
+#
+#  MorseCode.rb
+#  MorseConverter
+#
+#  Created by neige7 on 12/11/14.
+#  Copyright 2012年 neige7. All rights reserved.
+#
+
+
+class MorseCode
+    
+    def initialize
+        @kana = Array.new
+        @morse = Array.new
+        @alpha = Array.new
+    end
+
+end
