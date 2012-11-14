3d8954691eca6de4bf65549bf82277f357520925
ash1taka <ashitaka77@gmail.com>
2012-11-14 16:53:16 +0900

    アルファベット→モールス信号　変換機能

--- MorseConverter/Controller.rb
+++ MorseConverter/Controller.rb
@@ -10,14 +10,38 @@
 class Controller
     
     attr_accessor :button , :orgform , :kanaform, :alpform
+    attr_accessor :popup
     def initialize
         @mc = MorseCode.new
-        #puts "initialize"
     end
     
     def button_pressd(sender)
-        #puts "the button pressed!"
-        str = orgform.stringValue
-        kanaform.setStringValue(@mc.ja_morse(str))
+        
+        if(popup.indexOfSelectedItem == 0)
+            conv_morse
+        else
+            rev_conv_morse
+        end
+    end
+    
+    private
+    def conv_morse
+        begin
+            str = orgform.stringValue
+            kanaform.setStringValue(@mc.ja_morse(str))
+            alpform.setStringValue(@mc.al_morse(str))
+        rescue => ex
+            puts ex.message
+        end
+    
+    end
+
+    private
+    def rev_conv_morse
+        begin
+            str = orgform.stringValue
+        rescue => ex
+            puts ex.message
+        end
     end
 end
